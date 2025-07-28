45b74035871bdb8b5cc4f1252716cd6dc8815caa
Kevin Ritthaworn <rj.ritthaworn@icloud.com>
2025-07-28 19:03:15 -0400

    added route for select persona

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import CreateAccount from "./pages/CreateAccount";
+import SelectPersona from "./pages/SelectPersona";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/select-persona" element={<SelectPersona />} />
       </Routes>
     </Router>
   );
